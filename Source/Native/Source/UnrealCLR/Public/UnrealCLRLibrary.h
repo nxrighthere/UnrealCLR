6a8e92ae00788a5b6e40add10f9527e1b688ba53
nxrighthere <nxrighthere@gmail.com>
2020-06-17 20:12:06 +0500

    Migrate to LGPLv3 with a static linking exception

--- Source/Native/Source/UnrealCLR/Public/UnrealCLRLibrary.h
+++ Source/Native/Source/UnrealCLR/Public/UnrealCLRLibrary.h
@@ -1,31 +1,15 @@
 /*
- *  Copyright (c) 2020 Stanislav Denisov (nxrighthere@gmail.com)
+ * Copyright (c) 2020 Stanislav Denisov (nxrighthere@gmail.com)
  *
- *  Permission to use, copy, modify, and/or distribute this software free of
- *  charge is hereby granted, provided that the above copyright notice and this
- *  permission notice appear in all copies or portions of this software with
- *  respect to the following additional terms and conditions that apply to the
- *  software which distributed in a non-compiled and/or non-object files:
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 3 with a static linking exception which accompanies this
+ * distribution.
  *
- *  1. Without specific prior written permission of the copyright holder,
- *  this software is forbidden for rebranding, sublicensing, and the exploitation
- *  of its original brand to get payments in any form.
- *
- *  2. In accordance with DMCA (Digital Millennium Copyright Act), the copyright
- *  holder reserves exclusive permission to take down at any time any publicly
- *  available copy of this software in the original, partial, or modified form.
- *
- *  3. Any modifications that were made by third-parties to this software or its
- *  portions can be used by the copyright holder for any purposes, without any
- *  limiting factors and restrictions.
- *
- *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- *  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- *  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- *  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- *  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
- *  OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
- *  CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
  */
 
 #pragma once
