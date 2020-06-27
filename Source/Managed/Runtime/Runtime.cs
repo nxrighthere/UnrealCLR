b32249a363fde8be1cfdf2b7b8a609c444b8c72f
nxrighthere <nxrighthere@gmail.com>
2020-06-27 23:04:45 +0500

    Use a loaded assembly to get references

--- Source/Managed/Runtime/Runtime.cs
+++ Source/Managed/Runtime/Runtime.cs
@@ -14,7 +14,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Reflection;
 using System.Reflection.Emit;
 using System.Runtime.CompilerServices;
@@ -114,7 +113,7 @@ namespace UnrealEngine.Runtime {
 			}
 
 			if (!loaded) {
-				AssemblyName[] referencedAssemblies = Assembly.GetAssembly(type).GetReferencedAssemblies();
+				AssemblyName[] referencedAssemblies = plugin.assembly.GetReferencedAssemblies();
 
 				foreach (AssemblyName referencedAssembly in referencedAssemblies) {
 					if (referencedAssembly.Name == "UnrealEngine.Framework") {
@@ -124,13 +123,10 @@ namespace UnrealEngine.Runtime {
 							Type sharedClass = frameworkAssembly.GetType("UnrealEngine.Framework.Shared");
 
 							if ((bool)sharedClass.GetField("loaded", BindingFlags.NonPublic | BindingFlags.Static).GetValue(null) == false) {
-								if ((int)sharedClass.GetField("checksum", BindingFlags.NonPublic | BindingFlags.Static).GetValue(null) == sharedChecksum) {
+								if ((int)sharedClass.GetField("checksum", BindingFlags.NonPublic | BindingFlags.Static).GetValue(null) == sharedChecksum)
 									sharedClass.GetMethod("Load", BindingFlags.NonPublic | BindingFlags.Static).Invoke(null, new object[] { sharedFunctions });
-								} else {
+								else
 									Log(LogLevel.Fatal, "Unable to load framework from \"" + assemblyPath + "\" of type name \"" + typeName + "\" with method name \"" + methodName + "\"\r\nFramework version is incompatible with the runtime, recompile the project with an updated version");
-
-									Debugger.Break();
-								}
 							}
 						}
 
