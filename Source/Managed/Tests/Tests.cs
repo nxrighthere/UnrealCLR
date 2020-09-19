c17414c73b65b58c047e2e347c72d805f030d837
nxrighthere <nxrighthere@gmail.com>
2020-09-19 12:36:50 +0500

    Remove redundant actions in tests

--- Source/Managed/Tests/Tests.cs
+++ Source/Managed/Tests/Tests.cs
@@ -4,14 +4,12 @@ using UnrealEngine.Framework;
 
 namespace UnrealEngine.Tests {
 	public static class Main {
-		private static TestSystems testSystem;
 		private static ISystem runningSystem;
-		private static event Action OnBeginPlay;
-		private static event Action<float> OnTick;
-		private static event Action OnEndPlay;
 
 		public static void OnWorldPostBegin() {
 			Debug.Log(LogLevel.Display, "Hello, Unreal Engine!");
+
+			TestSystems testSystem = default;
 
 			if (World.GetActor<LevelScript>().GetEnum("Test Systems", ref testSystem))
 				Debug.AddOnScreenMessage(-1, 3.0f, Color.LightGreen, testSystem + " system started!");
@@ -19,120 +17,83 @@ namespace UnrealEngine.Tests {
 			switch (testSystem) {
 				case TestSystems.AssertionConsistency:
 					runningSystem = new AssertionConsistency();
-					OnBeginPlay += runningSystem.OnBeginPlay;
 					break;
 
 				case TestSystems.AudioPlayback:
 					runningSystem = new AudioPlayback();
-					OnBeginPlay += runningSystem.OnBeginPlay;
-					OnEndPlay += runningSystem.OnEndPlay;
 					break;
 
 				case TestSystems.BlueprintsExtensibility:
 					runningSystem = new BlueprintsExtensibility();
-					OnBeginPlay += runningSystem.OnBeginPlay;
-					OnEndPlay += runningSystem.OnEndPlay;
 					break;
 
 				case TestSystems.DebugVisualization:
 					runningSystem = new DebugVisualization();
-					OnBeginPlay += runningSystem.OnBeginPlay;
-					OnEndPlay += runningSystem.OnEndPlay;
 					break;
 
 				case TestSystems.DynamicEvents:
 					runningSystem = new DynamicEvents();
-					OnBeginPlay += runningSystem.OnBeginPlay;
-					OnTick += runningSystem.OnTick;
-					OnEndPlay += runningSystem.OnEndPlay;
 					break;
 
 				case TestSystems.DynamicsConsistency:
 					runningSystem = new DynamicsConsistency();
-					OnBeginPlay += runningSystem.OnBeginPlay;
-					OnTick += runningSystem.OnTick;
-					OnEndPlay += runningSystem.OnEndPlay;
 					break;
 
 				case TestSystems.ExceptionsConsistency:
 					runningSystem = new ExceptionsConsistency();
-					OnBeginPlay += runningSystem.OnBeginPlay;
-					OnEndPlay += runningSystem.OnEndPlay;
 					break;
 
 				case TestSystems.ExternalConsistency:
 					runningSystem = new ExternalConsistency();
-					OnBeginPlay += runningSystem.OnBeginPlay;
 					break;
 
 				case TestSystems.InstancedStaticMeshes:
 					runningSystem = new InstancedStaticMeshes();
-					OnBeginPlay += runningSystem.OnBeginPlay;
-					OnTick += runningSystem.OnTick;
-					OnEndPlay += runningSystem.OnEndPlay;
 					break;
 
 				case TestSystems.ObjectOrientedDesign:
 					runningSystem = new ObjectOrientedDesign();
-					OnBeginPlay += runningSystem.OnBeginPlay;
-					OnTick += runningSystem.OnTick;
-					OnEndPlay += runningSystem.OnEndPlay;
 					break;
 
 				case TestSystems.PhysicsSimulation:
 					runningSystem = new PhysicsSimulation();
-					OnBeginPlay += runningSystem.OnBeginPlay;
-					OnTick += runningSystem.OnTick;
-					OnEndPlay += runningSystem.OnEndPlay;
 					break;
 
 				case TestSystems.RadialForce:
 					runningSystem = new RadialForce();
-					OnBeginPlay += runningSystem.OnBeginPlay;
-					OnEndPlay += runningSystem.OnEndPlay;
 					break;
 
 				case TestSystems.RuntimeConsistency:
 					runningSystem = new RuntimeConsistency();
-					OnBeginPlay += runningSystem.OnBeginPlay;
 					break;
 
 				case TestSystems.SkeletalMeshes:
 					runningSystem = new SkeletalMeshes();
-					OnBeginPlay += runningSystem.OnBeginPlay;
-					OnEndPlay += runningSystem.OnEndPlay;
 					break;
 
 				case TestSystems.SpatialQueries:
 					runningSystem = new SpatialQueries();
-					OnBeginPlay += runningSystem.OnBeginPlay;
-					OnEndPlay += runningSystem.OnEndPlay;
 					break;
 
 				case TestSystems.StaticMeshes:
 					runningSystem = new StaticMeshes();
-					OnBeginPlay += runningSystem.OnBeginPlay;
-					OnTick += runningSystem.OnTick;
-					OnEndPlay += runningSystem.OnEndPlay;
 					break;
 
 				case TestSystems.TextureAssets:
 					runningSystem = new TextureAssets();
-					OnBeginPlay += runningSystem.OnBeginPlay;
-					OnEndPlay += runningSystem.OnEndPlay;
 					break;
 
 				default:
 					break;
 			}
 
-			OnBeginPlay?.Invoke();
+			runningSystem.OnBeginPlay();
 		}
 
-		public static void OnWorldPrePhysicsTick(float deltaTime) => OnTick?.Invoke(deltaTime);
+		public static void OnWorldPrePhysicsTick(float deltaTime) => runningSystem.OnTick(deltaTime);
 
 		public static void OnWorldEnd() {
-			OnEndPlay?.Invoke();
+			runningSystem.OnEndPlay();
 
 			Debug.Log(LogLevel.Display, "See you soon, Unreal Engine!");
 		}
