aaf008c4ad9f6c12ce3012ef8e11c2d91083e591
nxrighthere <nxrighthere@gmail.com>
2020-10-07 23:21:43 +0500

    Update unit tests

--- Source/Managed/Tests/VirtualReality.cs
+++ Source/Managed/Tests/VirtualReality.cs
@@ -4,42 +4,44 @@ using System.Numerics;
 using UnrealEngine.Framework;
 
 namespace UnrealEngine.Tests {
-    public class VirtualReality : ISystem {
-        private Pawn pawnVR;
-        private SceneComponent body;
-        private MotionControllerComponent leftHand;
-        private MotionControllerComponent rightHand;
+	public class VirtualReality : ISystem {
+		private Pawn pawnVR;
+		private SceneComponent body;
+		private MotionControllerComponent leftHand;
+		private MotionControllerComponent rightHand;
 
-        public VirtualReality() {
-            pawnVR = new("PawnVR");
-            body = new(pawnVR, "Root");
-            leftHand = new(pawnVR, "LeftHand");
-            rightHand = new(pawnVR, "RightHand");
-        }
+		public VirtualReality() {
+			pawnVR = new("PawnVR");
+			body = new(pawnVR, "Root");
+			leftHand = new(pawnVR, "LeftHand");
+			rightHand = new(pawnVR, "RightHand");
+		}
 
-        public void OnBeginPlay() {
-            Camera mainCamera = World.GetActor<Camera>("MainCamera");
+		public void OnBeginPlay() {
+			Camera mainCamera = World.GetActor<Camera>("MainCamera");
 
-            mainCamera.GetComponent<CameraComponent>().LockToHeadMountedDisplay = true;
+			mainCamera.GetComponent<CameraComponent>().LockToHeadMountedDisplay = true;
 
-            PlayerController playerController = World.GetFirstPlayerController();
+			PlayerController playerController = World.GetFirstPlayerController();
 
-            playerController.SetViewTarget(mainCamera);
-            playerController.Possess(pawnVR);
+			playerController.SetViewTarget(mainCamera);
+			playerController.Possess(pawnVR);
 
-            leftHand.DisplayDeviceModel = true;
-            leftHand.SetTrackingMotionSource("Left");
+			leftHand.DisplayDeviceModel = true;
+			leftHand.SetTrackingMotionSource("Left");
 
-            rightHand.DisplayDeviceModel = true;
-            rightHand.SetTrackingMotionSource("Right");
-        }
+			rightHand.DisplayDeviceModel = true;
+			rightHand.SetTrackingMotionSource("Right");
+		}
 
-        public void OnTick(float deltaTime) {
-            Debug.AddOnScreenMessage(1, 3.0f, Color.Yellow, "Head-mounted display is connected: " + HeadMountedDisplay.IsConnected);
-            Debug.AddOnScreenMessage(2, 3.0f, Color.Yellow, "Device enabled: " + HeadMountedDisplay.Enabled);
-            Debug.AddOnScreenMessage(3, 3.0f, Color.Yellow, "Device name: " + HeadMountedDisplay.DeviceName);
-        }
+		public void OnTick(float deltaTime) {
+			Debug.AddOnScreenMessage(1, 3.0f, Color.Yellow, "Head-mounted display is connected: " + HeadMountedDisplay.IsConnected);
+			Debug.AddOnScreenMessage(2, 3.0f, Color.Yellow, "Device enabled: " + HeadMountedDisplay.Enabled);
+			Debug.AddOnScreenMessage(3, 3.0f, Color.Yellow, "Device name: " + HeadMountedDisplay.DeviceName);
+			Debug.AddOnScreenMessage(4, 3.0f, Color.Orange, "Left motion controller is tracked: " + leftHand.IsTracked);
+			Debug.AddOnScreenMessage(5, 3.0f, Color.Orange, "Right motion controller is tracked: " + rightHand.IsTracked);
+		}
 
-        public void OnEndPlay() => Debug.ClearOnScreenMessages();
-    }
+		public void OnEndPlay() => Debug.ClearOnScreenMessages();
+	}
 }
