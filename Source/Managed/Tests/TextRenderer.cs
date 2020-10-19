794e7b8408efcac04b76861f73568c18a322c47c
nxrighthere <nxrighthere@gmail.com>
2020-10-19 12:33:04 +0500

    Update unit tests

--- Source/Managed/Tests/TextRenderer.cs
+++ Source/Managed/Tests/TextRenderer.cs
@@ -4,26 +4,27 @@ using UnrealEngine.Framework;
 
 namespace UnrealEngine.Tests {
 	public class TextRenderer : ISystem {
-        private Actor actor;
-        private TextRenderComponent textRenderComponent;
-        private Font font;
-        private Material material;
+		private Actor actor;
+		private TextRenderComponent textRenderComponent;
+		private Font font;
+		private Material material;
 
 		public TextRenderer() {
-            actor = new("TextActor");
-            textRenderComponent = new(actor);
-            font = Font.Load("/Game/Tests/PlayFont");
-            material = Material.Load("/Game/Tests/PlayFontMaterial");
+			actor = new("TextActor");
+			textRenderComponent = new(actor);
+			font = Font.Load("/Game/Tests/PlayFont");
+			material = Material.Load("/Game/Tests/PlayFontMaterial");
 		}
 
 		public void OnBeginPlay() {
-            textRenderComponent.SetFont(font);
-            textRenderComponent.SetTextMaterial(material);
-            textRenderComponent.SetText("WELCOME TO THE WORLD");
-            textRenderComponent.SetHorizontalAlignment(HorizontalTextAligment.Center);
-            textRenderComponent.SetVerticalAlignment(VerticalTextAligment.Center);
-            textRenderComponent.SetWorldLocation(new Vector3(200.0f, 0.0f, 0.0f));
-            textRenderComponent.SetWorldRotation(Maths.Euler(0.0f, 0.0f, 180.0f));
+			textRenderComponent.SetFont(font);
+			textRenderComponent.SetTextMaterial(material);
+			textRenderComponent.SetText("WELCOME TO THE WORLD");
+			textRenderComponent.SetHorizontalSpacingAdjustment(2.0f);
+			textRenderComponent.SetHorizontalAlignment(HorizontalTextAligment.Center);
+			textRenderComponent.SetVerticalAlignment(VerticalTextAligment.Center);
+			textRenderComponent.SetWorldLocation(new Vector3(200.0f, 0.0f, 0.0f));
+			textRenderComponent.SetWorldRotation(Maths.Euler(0.0f, 0.0f, 180.0f));
 		}
 	}
 }
