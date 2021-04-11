79a05742d31f9d1ba6dbb6ab6adf2fc1faa8aec5
nxrighthere <nxrighthere@gmail.com>
2021-04-11 16:39:38 +0200

    Add level name check for tests

--- Source/Managed/Tests/Tests.cs
+++ Source/Managed/Tests/Tests.cs
@@ -9,41 +9,43 @@ namespace UnrealEngine.Tests {
 		public static void OnWorldPostBegin() {
 			Debug.Log(LogLevel.Display, "Hello, Unreal Engine!");
 
-			TestSystems testSystem = default;
+			if (World.CurrentLevelName == "Tests") {
+				TestSystems testSystem = default;
 
-			if (World.GetActor<LevelScript>().GetEnum("Test Systems", ref testSystem))
-				Debug.AddOnScreenMessage(-1, 3.0f, Color.LightGreen, testSystem + " system started!");
+				if (World.GetActor<LevelScript>().GetEnum("Test Systems", ref testSystem))
+					Debug.AddOnScreenMessage(-1, 3.0f, Color.LightGreen, testSystem + " system started!");
 
-			runningSystem = testSystem switch {
-				TestSystems.AssertionConsistency => new AssertionConsistency(),
-				TestSystems.AudioPlayback => new AudioPlayback(),
-				TestSystems.BlueprintsExtensibility => new BlueprintsExtensibility(),
-				TestSystems.DebugVisualization => new DebugVisualization(),
-				TestSystems.DynamicEvents => new DynamicEvents(),
-				TestSystems.DynamicsConsistency => new DynamicsConsistency(),
-				TestSystems.ExceptionsConsistency => new ExceptionsConsistency(),
-				TestSystems.ExternalConsistency => new ExternalConsistency(),
-				TestSystems.InstancedStaticMeshes => new InstancedStaticMeshes(),
-				TestSystems.ObjectOrientedDesign => new ObjectOrientedDesign(),
-				TestSystems.PhysicsSimulation => new PhysicsSimulation(),
-				TestSystems.RadialForce => new RadialForce(),
-				TestSystems.RuntimeConsistency => new RuntimeConsistency(),
-				TestSystems.SkeletalMeshes => new SkeletalMeshes(),
-				TestSystems.SpatialQueries => new SpatialQueries(),
-				TestSystems.StaticMeshes => new StaticMeshes(),
-				TestSystems.TextRenderer => new TextRenderer(),
-				TestSystems.TextureAssets => new TextureAssets(),
-				TestSystems.VirtualReality => new VirtualReality(),
-				_ => throw new Exception("Unknown system")
-			};
+				runningSystem = testSystem switch {
+					TestSystems.AssertionConsistency => new AssertionConsistency(),
+					TestSystems.AudioPlayback => new AudioPlayback(),
+					TestSystems.BlueprintsExtensibility => new BlueprintsExtensibility(),
+					TestSystems.DebugVisualization => new DebugVisualization(),
+					TestSystems.DynamicEvents => new DynamicEvents(),
+					TestSystems.DynamicsConsistency => new DynamicsConsistency(),
+					TestSystems.ExceptionsConsistency => new ExceptionsConsistency(),
+					TestSystems.ExternalConsistency => new ExternalConsistency(),
+					TestSystems.InstancedStaticMeshes => new InstancedStaticMeshes(),
+					TestSystems.ObjectOrientedDesign => new ObjectOrientedDesign(),
+					TestSystems.PhysicsSimulation => new PhysicsSimulation(),
+					TestSystems.RadialForce => new RadialForce(),
+					TestSystems.RuntimeConsistency => new RuntimeConsistency(),
+					TestSystems.SkeletalMeshes => new SkeletalMeshes(),
+					TestSystems.SpatialQueries => new SpatialQueries(),
+					TestSystems.StaticMeshes => new StaticMeshes(),
+					TestSystems.TextRenderer => new TextRenderer(),
+					TestSystems.TextureAssets => new TextureAssets(),
+					TestSystems.VirtualReality => new VirtualReality(),
+					_ => throw new Exception("Unknown system")
+				};
+			}
 
-			runningSystem.OnBeginPlay();
+			runningSystem?.OnBeginPlay();
 		}
 
-		public static void OnWorldPrePhysicsTick(float deltaTime) => runningSystem.OnTick(deltaTime);
+		public static void OnWorldPrePhysicsTick(float deltaTime) => runningSystem?.OnTick(deltaTime);
 
 		public static void OnWorldEnd() {
-			runningSystem.OnEndPlay();
+			runningSystem?.OnEndPlay();
 
 			Debug.Log(LogLevel.Display, "See you soon, Unreal Engine!");
 		}
